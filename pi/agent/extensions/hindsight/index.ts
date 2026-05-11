13d4deecbe3c52f11887d0c85c04de2ed82a5cc7
Avery Crespi <averycrespi@gmail.com>
2026-05-10 20:37:33 -0700

    feat(hindsight): add memory diagnostics

--- pi/agent/extensions/hindsight/index.ts
+++ pi/agent/extensions/hindsight/index.ts
@@ -2,12 +2,17 @@ import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
 import { registerConfigCommand } from "../_shared/config.ts";
 import { HindsightClient } from "./client.ts";
 import { DEFAULT_HINDSIGHT_CONFIG, loadHindsightConfig } from "./config.ts";
+import { registerHindsightDoctorCommand } from "./doctor.ts";
 import { registerHindsightTool } from "./tools.ts";
 
 export default function (pi: ExtensionAPI) {
   const client = new HindsightClient(DEFAULT_HINDSIGHT_CONFIG);
 
   registerHindsightTool(pi, { client, loadConfig: loadHindsightConfig });
+  registerHindsightDoctorCommand(pi, {
+    client,
+    loadConfig: loadHindsightConfig,
+  });
   registerConfigCommand(pi, {
     extensionName: "hindsight",
     loadConfig: loadHindsightConfig,
